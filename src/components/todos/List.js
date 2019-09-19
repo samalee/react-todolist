99f4f5b7c9e52af41a0de91a4e53bfef0e1a1fc7
Samantha Malay <samaleemalay@gmail.com>
2019-09-18 20:00:30 -0600

    testing

--- src/components/todos/List.js
+++ src/components/todos/List.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import { tsPropertySignature, isTemplateElement } from '@babel/types';
 
-const List = ( name, items ) => (
+const List = ({ name, items }) => (
   <div>
     <h2>{name}</h2>
     <ul>
-      { items.map( item =>) <li key={item.id}> {ite.name} </li> }
+      { items.map( item => <li key={item.id}>{item.name}</li>)}
     </ul>
   </div>
 )
